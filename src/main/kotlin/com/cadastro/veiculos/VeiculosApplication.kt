ef434cd0436188a4e71f58bec4f4553a7b5669e0
Vinicius Salvador Ribeiro <viniciusribeiro113@gmail.com>
2022-04-20 11:24:04 -0300

    Project intialized

---
+++ src/main/kotlin/com/cadastro/veiculos/VeiculosApplication.kt
@@ -0,0 +1,11 @@
+package com.cadastro.veiculos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VeiculosApplication
+
+fun main(args: Array<String>) {
+	runApplication<VeiculosApplication>(*args)
+}
